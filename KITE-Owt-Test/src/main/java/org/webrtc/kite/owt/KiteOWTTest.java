d098a2985cba04d31cd974381e7b41d1eed78303
ManuCosmo <emmanuel.andre@cosmosoftware.io>
2019-11-20 11:29:14 +0800

    Added room manager

--- KITE-Owt-Test/src/main/java/org/webrtc/kite/owt/KiteOWTTest.java
+++ KITE-Owt-Test/src/main/java/org/webrtc/kite/owt/KiteOWTTest.java
@@ -1,6 +1,7 @@
 package org.webrtc.kite.owt;
 
 import io.cosmosoftware.kite.steps.*;
+import io.cosmosoftware.kite.util.TestUtils;
 import org.webrtc.kite.owt.checks.AllVideoCheck;
 import org.webrtc.kite.owt.checks.FirstVideoCheck;
 import org.webrtc.kite.owt.pages.OWTPage;
@@ -10,6 +11,8 @@ import org.webrtc.kite.tests.KiteBaseTest;
 import org.webrtc.kite.tests.TestRunner;
 
 import javax.json.JsonObject;
+
+import static org.webrtc.kite.Utils.getStackTrace;
 
 public class KiteOWTTest extends KiteBaseTest {
 
@@ -27,35 +30,40 @@ public class KiteOWTTest extends KiteBaseTest {
 
   @Override
   public void populateTestSteps(TestRunner runner) {
-    final OWTPage owtPage = new OWTPage(runner);
-    runner.addStep(new OpenUrlStep(runner, url));
-    runner.addStep(new FirstVideoCheck(runner, owtPage));
+    try {
+      final OWTPage owtPage = new OWTPage(runner);
+      String roomUrl = getRoomManager().getRoomUrl()  + "&username=user" + TestUtils.idToString(runner.getId());
+      runner.addStep(new OpenUrlStep(runner, roomUrl));
+      runner.addStep(new FirstVideoCheck(runner, owtPage));
+  
+      if (this.takeScreenshotForEachTest()) {
+        runner.addStep(new ScreenshotStep(runner));
+      }
+      runner.addStep(new AllVideoCheck(runner, getMaxUsersPerRoom(), owtPage));
+      if (this.getStats()) {
+        runner.addStep(new GetStatsStep(runner, getStatsConfig, owtPage));
+      }
+  
+      if (this.allureCharts) {
+        runner.addStep(new StartGetStatsStep(runner, getChartsConfig));
+      }
+  
+      if (this.takeScreenshotForEachTest()) {
+        runner.addStep(new ScreenshotStep(runner));
+      }
+      if (this.meetingDuration > 0) {
+        runner.addStep(new StayInMeetingStep(runner, meetingDuration));
+      }
+  
+      if (this.allureCharts) {
+        runner.addStep(new StopGetStatsStep(runner));
+        runner.addStep(new GenerateChartsStep(runner, getChartsConfig, getTestJar()));
+      }
 
-    if (this.takeScreenshotForEachTest()) {
-      runner.addStep(new ScreenshotStep(runner));
-    }
-    runner.addStep(new AllVideoCheck(runner, getMaxUsersPerRoom(), owtPage));
-    if (this.getStats()) {
-      runner.addStep(new GetStatsStep(runner, getStatsConfig, owtPage));
-    }
 
-    if (this.allureCharts) {
-      runner.addStep(new StartGetStatsStep(runner, getChartsConfig));
+    } catch (Exception e) {
+      logger.error(getStackTrace(e));
     }
-
-    if (this.takeScreenshotForEachTest()) {
-      runner.addStep(new ScreenshotStep(runner));
-    }
-    if (this.meetingDuration > 0) {
-      runner.addStep(new StayInMeetingStep(runner, meetingDuration));
-    }
-
-    if (this.allureCharts) {
-      runner.addStep(new StopGetStatsStep(runner));
-      runner.addStep(new GenerateChartsStep(runner, getChartsConfig, getTestJar()));
-    }
-    
-    
   }
 
 }
